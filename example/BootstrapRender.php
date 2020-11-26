e66a8a8715530ae6317f0c497fb1af3f7033e596
EncryptSL <Rydlos123@gmail.com>
2020-11-26 19:46:46 +0100

    Update BootstrapRender.php

--- example/BootstrapRender.php
+++ example/BootstrapRender.php
@@ -16,13 +16,13 @@ class BootstrapRender extends DefaultFormRenderer
         $pair = $this->getWrapper('pair container');
         $pair->addHtml($this->renderControl($control));
         $pair->addHtml($this->renderLabel($control));
-		    $pair->class($this->getValue($control->isRequired() ? 'pair .required' : 'pair .optional'), true);
-		    $pair->class($control->hasErrors() ? $this->getValue('pair .error') : null, true);
-		    $pair->class($control->getOption('class'), true);
-		    if (++$this->counter % 2) {
-			    $pair->class($this->getValue('pair .odd'), true);
-		    }
-		    $pair->id = $control->getOption('id');
-		    return $pair->render(0);
+	$pair->class($this->getValue($control->isRequired() ? 'pair .required' : 'pair .optional'), true);
+	$pair->class($control->hasErrors() ? $this->getValue('pair .error') : null, true);
+	$pair->class($control->getOption('class'), true);
+	if (++$this->counter % 2) {
+		$pair->class($this->getValue('pair .odd'), true);
+	}
+	$pair->id = $control->getOption('id');
+	return $pair->render(0);
     }
 }
