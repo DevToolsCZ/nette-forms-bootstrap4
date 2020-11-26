640a99eaee552a305e1a5c031cbf51168f2485a8
EncryptSL <Rydlos123@gmail.com>
2020-11-26 14:09:44 +0100

    Create BootstrapRender.php
    BootstrapRender.php is Extends of Nette\Forms\Rendering\DefaultFormRenderer.php

---
+++ example/BootstrapRender.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Forms;
+
+use Nette\Forms\IControl;
+use Nette\Forms\Rendering\DefaultFormRenderer;
+
+class BootstrapRender extends DefaultFormRenderer 
+{
+    public function renderPair(IControl $control): string
+    {
+        parent::renderPair($control);
+
+        $pair = $this->getWrapper('pair container');
+        $pair->addHtml($this->renderControl($control));
+        $pair->addHtml($this->renderLabel($control));
+		    $pair->class($this->getValue($control->isRequired() ? 'pair .required' : 'pair .optional'), true);
+		    $pair->class($control->hasErrors() ? $this->getValue('pair .error') : null, true);
+		    $pair->class($control->getOption('class'), true);
+		    if (++$this->counter % 2) {
+			    $pair->class($this->getValue('pair .odd'), true);
+		    }
+		    $pair->id = $control->getOption('id');
+		    return $pair->render(0);
+    }
+}
