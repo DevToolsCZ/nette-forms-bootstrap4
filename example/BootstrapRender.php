2f2f85295aef55b9c9ea82a4a314ed5f135182af
EncryptSL <Rydlos123@gmail.com>
2020-12-14 03:50:41 +0100

    Update BootstrapRender.php
    Wrapper moved from FormFactory to Renderrer

--- example/BootstrapRender.php
+++ example/BootstrapRender.php
@@ -9,6 +9,16 @@ use Nette\Forms\Rendering\DefaultFormRenderer;
 
 class BootstrapRender extends DefaultFormRenderer 
 {
+     public function __construct() 
+     {
+        $this->wrappers['controls']['container'] = null;
+        $this->wrappers['pair']['container'] = 'div class="form-floating has-validation mb-3"';
+        $this->wrappers['pair']['.error'] = 'has-danger';
+        $this->wrappers['control']['description'] = 'span class=form-text';
+        $this->wrappers['control']['errorcontainer'] = 'span class=invalid-feedback';
+        $this->wrappers['control']['.error'] = 'invalid-feedback';
+    }
+
     public function renderPair(IControl $control): string
     {
         parent::renderPair($control);
